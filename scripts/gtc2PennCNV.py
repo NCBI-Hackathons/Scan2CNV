ad1ea77e49cfd7e993a82e08b42ff35b0b2eb921
Karlins <karlinser@nci-01940802-ml.nih.gov>
2017-03-21 16:50:08 -0400

    fixes issue #18

--- scripts/gtc2PennCNV.py
+++ scripts/gtc2PennCNV.py
@@ -1,6 +1,8 @@
 import sys
 import os
-sys.path.append('../BeadArrayFiles/module')
+
+scriptDir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(scriptDir + '/../BeadArrayFiles/module'))
 from IlluminaBeadArrayFiles import GenotypeCalls, BeadPoolManifest, code2genotype
 
 
