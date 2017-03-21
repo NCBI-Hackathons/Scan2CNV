cdbef971ae72102d963bb190f84e82a007814836
ekarlins <ekarlins@gmail.com>
2017-03-21 15:29:48 -0400

    changed dir in bash file

--- scripts/run_gtc2PennCNV_NciCluster.sh
+++ scripts/run_gtc2PennCNV_NciCluster.sh
@@ -3,4 +3,4 @@
 
 module load python/2.7.5
 cd /out/dir
-python /path/to/repo/scripts/gtc2plink.py /path/to/file.gtc /path/to/manifest.bpm out/outFile.txt
+python /DCEG/repo/scripts/gtc2plink.py /path/to/file.gtc /path/to/manifest.bpm out/outFile.txt
