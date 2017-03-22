b1e32f51736d5792c84671a72a239684ea864a05
ngiangre <nick.giangreco@gmail.com>
2017-03-21 23:51:25 -0400

    updated R script-assigns cnv state to regions and adding files from testing

--- R/gsrcCNVcalling.R
+++ R/gsrcCNVcalling.R
@@ -6,6 +6,7 @@
 ########## 
 
 args <- commandArgs(TRUE)
+#args<-c("/Users/nickgiangreco/GitHub/Global_Screening_Arrays/files","/Users/nickgiangreco/GitHub/Global_Screening_Arrays/files")
 
 ##Install/load libraries
 #source("https://bioconductor.org/biocLite.R")
@@ -16,8 +17,8 @@ library(gsrc)
 
 ##load PennCNV input file
 #filewpath<-"/Users/nickgiangreco/GitHub/Global_Screening_Arrays/files/pennTest2.txt"
-files<-list.files(path=args[1],pattern="pennTest",full.names=T)
-filenames<-list.files(path=args[1],pattern="pennTest",full.names=F)
+files<-list.files(path=args[1],pattern="*PennCNV*",full.names=T)
+filenames<-list.files(path=args[1],pattern="*PennCNV*",full.names=F)
 for(f in 1:length(files)){
 
 #assigning file with full path
@@ -28,7 +29,7 @@ tmp<-filenames[f]
 sample.name<-unlist(strsplit(tmp,"\\."))[1]
 
 #reading file
-file.df<-read.table(filewpath,sep="\t",header=T)
+file.df<-read.delim(filewpath,sep="\t",header=T)
 
 #making custom list to comply to
 #"gsrc" cnv calling
@@ -37,9 +38,9 @@ file.df<-read.table(filewpath,sep="\t",header=T)
 dat<-list("snps" = c(),"chr" = c(), "pos" = c(), "baf" = c(), "rratio" = c(), "samples" = c(), "geno" = c())
 dat$snps<-file.df$Name
 dat$chr<-file.df$Chr
-dat$pos<-file.df$Pos
-dat$baf<-file.df$BAF
-dat$rratio<-file.df$LRR
+dat$pos<-file.df$Position
+dat$baf<-file.df$B.Allele.Freq
+dat$rratio<-file.df$Log.R.Ratio
 dat$samples<-sample.name
 dat$geno<-file.df$Gtype
 
@@ -52,30 +53,35 @@ dup<-0.1
 cnv.call<-cnv(seg,del=del,dup=dup) 
 
 #Output to bedgraph
-#outDir<-"/files"
+#outDir<-"/Users/nickgiangre/GitHub/Gobal_Screening_Arrays/files/output"
 outDir<-args[2]
 
-#Parsing output
+#Making data frames
 cna.bed<-data.frame("chr"=cnv.call$cna$chrom,"s"=cnv.call$cna$loc.start,"e"=cnv.call$cna$loc.end,
                "num.probes"=cnv.call$cna$num.mark,"avg.probe.val"=cnv.call$cna$seg.mean)
 all.snp.bed<-data.frame("chr"=cnv.call$chr,"s"=cnv.call$pos,"geno"=cnv.call$geno,"state"=cnv.call$cnv)
 
+#assigning cnv state from individual probes to regions
 chrs<-names(table(cna.bed$chr))
 cna.bed$state<-"NULL"
-
 for(i in chrs){
-  all.snp.s<-sort(subset(all.snp.bed,chr==i)$s,decreasing=F)
-  cna.bed.s<-sort(subset(cna.bed,chr==i)$s,decreasing=F)
-  for(j in cna.bed.s){
-    if(length(which(j < all.snp.s))>0)next
-    inds.s<-which(j >= all.snp.s)
-    for(k in 1:length(inds.s)){
-      rn<-rownames(subset(cna.bed,chr==i)[inds.s,])
-      cna.bed[rn,]<-all.snp.bed[rn,]$state
-    }
+  #cat("\n",i)
+  #get regions and probes in chromosome
+  sub.cna.bed<-subset(cna.bed,chr==i)
+  st<-sub.cna.bed$s
+  en<-sub.cna.bed$e
+  #cat("\n\t",length(st))
+  #then looping for each cna start
+  for(j in 1:length(st)){
+    rn<-rownames(subset(sub.cna.bed, chr==i & s==s[j]))
+    states<-subset(all.snp.bed, chr==i & s >= st[j] & s <= en[j])$state
+    if(mean(states)%%1==0 | length(states)==0){
+      cna.bed[rn,"state"]<-0}else{
+        cna.bed[rn,"state"]<-states[1]}
   }
 }
 
-write.table(cna.bed,file=paste0(path,outDir,"/",sample.name,"_gsrcCNVcall.bed"),
+#Output
+write.table(cna.bed,file=paste0(outDir,"/",sample.name,"_gsrcCNVcall.bed"),
             quote=F,row.names=F,col.names=F)
 }
